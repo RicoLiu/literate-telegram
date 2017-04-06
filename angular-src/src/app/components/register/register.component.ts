2abefe79d10462ee994c0eaa32cb2afa98517b10
RicoLiu <2869183609@qq.com>
2017-04-06 20:40:39 +0800

    add Register

--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages'
+import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+    private validateService: ValidateService,
+    private flashMessagesService: FlashMessagesService,
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    };
+
+    //Required
+    if (!this.validateService.validateRegister(user)) {
+      this.flashMessagesService.show('fill in all fileds', { cssClass: 'alert-danger', timeout: 2000 });
+      return false;
+    }
+
+    if (!this.validateService.validateEmail(user.email)) {
+      this.flashMessagesService.show('worng email', { cssClass: 'alert-danger', timeout: 2000 });
+      return false;
+    }
+
+    //Register User
+    this.authService.registerUser(user).subscribe(data => {
+      if (data.success) {
+        this.flashMessagesService.show('Registered Success', { cssClass: 'alert-success', timeout: 2000 });
+        this.router.navigate(['/login']);
+      } else {
+        this.flashMessagesService.show('Registered Failed', { cssClass: 'alert-danger', timeout: 2000 });
+        this.router.navigate(['/register']);
+      }
+    });
   }
 
 }
