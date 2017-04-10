d39615fc67029fe9c6432ae3e841f410d65b13b3
RicoLiu <2869183609@qq.com>
2017-04-10 19:46:23 +0800

    登录&登出

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../../services/auth.service'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: String;
+  password: String;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: FlashMessagesService
+    ) { }
 
   ngOnInit() {
   }
 
+  onLoginSubmit() {
+    const user = {
+      username: this.username,
+      password: this.password
+    }
+
+    this.authService.authenticateUser(user).subscribe(data => {
+      console.log(data)
+      if (data.success) {
+        this.authService.storeUserData(data.token, data.user);
+        this.flashMessage.show('success login', {cssClass: 'alert-success', timeout: 2000});
+        this.router.navigate(['dashboard']);
+      } else {
+        this.flashMessage.show(data.msg, {cssClass: 'alert-danger', timeout: 2000});
+        this.router.navigate(['login']);
+      }
+
+    })
+  }
 }
